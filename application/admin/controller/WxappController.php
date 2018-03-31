f70ba646da2e4c90c97e93960ddb290bbe5c7408
gumeijun <751415315@qq.com>
2018-03-31 23:52:31 +0800

    增加新功能

--- application/admin/controller/WxappController.php
+++ application/admin/controller/WxappController.php
@@ -3,16 +3,14 @@ namespace app\admin\controller;
 use wxsaas\classs\WxappAcount;
 use wxsaas\controller\AdminBaseController;
 use think\Config;
+use wxsaas\model\AccountWxapp;
 use wxsaas\model\Modules;
 class WxappController extends AdminBaseController
 {
     public function index()
     {
-        $model = new Modules();
-        $moduleInfo = $model->module_fetch('shuozhuo_benyilipin');
-        if(isset($moduleInfo['error'])){
-            var_dump($moduleInfo['msg']);die;
-        }
+        $lists = AccountWxapp::all();
+        $this->assign('lists',$lists);
         return $this->fetch();
     }
     public function login(){
@@ -33,6 +31,22 @@ class WxappController extends AdminBaseController
         if($id){
             $this->redirect("wxapp/manage",['id'=>$id]);
         }
+    }
+    public function detail(){
+        $id = input('id');
+        $module = AccountWxapp::get(['id'=>$id]);
+        $this->assign('info',$module);
+        $lists = unserialize('a:1:{s:11:"yyf_company";a:2:{s:4:"name";s:11:"yyf_company";s:7:"version";s:4:"21.0";}}');
+        $wxappLists = [];
+        foreach($lists as $name=>$item){
+            $wxappInfo = Modules::get(['name'=>$name]);
+            $wxapp['name'] = $name;
+            $wxapp['title'] = $wxappInfo->title;
+            $wxapp['url'] = 'addons.php/'.$name."/web.index";
+            $wxappLists[]= $wxapp;
+        }
+        var_dump($wxappLists);
+        return $this->fetch();
     }
     public function manage(){
         echo request()->get('id');
